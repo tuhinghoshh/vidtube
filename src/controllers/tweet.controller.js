3ebb896f92e78d2e3532521ee0f186d875e240a2
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2025-02-05 23:37:08 +0530

    tweetroutes

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,22 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const createTweet = asyncHandler(async (req, res) => {
+  //TODO: create tweet
+});
+
+export const getUserTweets = asyncHandler(async (req, res) => {
+  // TODO: get user tweets
+});
+
+export const updateTweet = asyncHandler(async (req, res) => {
+  //TODO: update tweet
+});
+
+export const deleteTweet = asyncHandler(async (req, res) => {
+  //TODO: delete tweet
+});
